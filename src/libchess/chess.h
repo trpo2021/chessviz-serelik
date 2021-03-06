6708016753826dff8e240fa7b8f4ba7618ac8a7a
Zdanovich Ivan <Zdanowitch.Iv@yandex.ru>
2021-03-07 00:27:27 +0700

    Add build.yml for CI

--- src/libchess/chess.h
+++ src/libchess/chess.h
@@ -4,6 +4,10 @@
 
 void init_chess_board();
 void print_board();
-int figure_move(const int& start_x, const int& start_y, const int& final_x, const int& final_y);
+int figure_move(
+        const int& start_x,
+        const int& start_y,
+        const int& final_x,
+        const int& final_y);
 
 #endif
